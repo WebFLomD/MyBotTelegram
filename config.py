a409e8f3c598bf8bd6bfcc1acb4f9a732dcc1db9
Alexander Zaharov <113933758+WebFLomD@users.noreply.github.com>
2024-10-02 14:37:24 +0300

    Добавлен API WEATHERAPI_KEY для получение данные о погоде в городе

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 # Токет Telegram
-TOKEN_BOT_TELEGRAM = 'YOUR_TOKEN'
+TOKEN_BOT_TELEGRAM = 'YOUR_TOKEN' # <--- Вставьте ваш Токен
 
 # Список администраторов (ID пользователей)
 ADMIN_IDS = []
+
+# WEATHERAPI API KEY (https://www.weatherapi.com/)
+WEATHERAPI_KEY = 'YOUR_WEATHERAPI_KEY' # <--- Вставьте ваш WEATHERAPI_KEY (https://www.weatherapi.com/)
